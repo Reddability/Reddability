a28ef8507b6758ede6c74890ca66df01d60091e7
Andreas Backx <andreas@backx.org>
2014-06-14 12:38:24 +0200

    Removed favicon editing.

--- js/main.js
+++ js/main.js
@@ -1,9 +1,4 @@
 $(document).ready(function() {
-    var favicon = $('link[type="image/x-icon"');
-    $(favicon).attr('rel', 'shortcut icon');
-    $(favicon).attr('href', chrome.extension.getURL('img/favicon.ico'));
-    console.log('Favicon has been set.');
-
 	$('.side #search input[name="q"]').attr('autocomplete', 'off');
 	console.log('Search autocomplete has been turned off.');
 });
