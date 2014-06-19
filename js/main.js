df934dc6a0cecbc89713fd69e39ad3677dd0b810
Andreas Backx <andreas@backx.org>
2014-06-19 13:33:01 +0200

    Removed logs in Javascript and added a lastError check.

--- js/main.js
+++ js/main.js
@@ -6,32 +6,21 @@ var sidebar = $('.side');
 
 $(document).ready(function() {
 	$('.side #search input[name="q"]').attr('autocomplete', 'off');
-	console.log('Search autocomplete has been turned off.');
 
-	console.log('Adding hiding sidebar option to header.');
 	$('.flat-list.hover li').append('<a href="#" id="sidebar-hide" class="pref-lang">' + sidebarVisibleText + '</a>');
 
-	console.log('Hiding/showing sidebar based on previous visits.');
 	updateSidebar();
 
-	console.log('Seting onclickhandler.');
 	$(sidebarHide).on('click', setSidebar);
-	console.log('Onclickhandler set.');
 });
 
 function setSidebar(hide) {
-	console.log('setSidebar with ' + hide + ', type: ' + (typeof hide));
 	var isHidden = $(sidebarHide).hasClass(sidebarHiddenClass);
-	console.log(isHidden + ' || (' + hide + ' === false) \n    ' +
-		isHidden + ' || ' + (hide === false) + ' \n        ' +
-		(isHidden || (hide === false)));
 	if(isHidden || hide === false) {
-		console.log('Showing sidebar...');
 		$(sidebarHide).text(sidebarVisibleText);
 		$(sidebar).show();
 		$(sidebarHide).removeClass(sidebarHiddenClass);
 	} else {
-		console.log('Hiding sidebar...');
 		$(sidebarHide).text(sidebarHiddenText);
 		$(sidebar).hide();
 		$(sidebarHide).addClass(sidebarHiddenClass);
@@ -41,16 +30,16 @@ function setSidebar(hide) {
 
 function saveSidebarSettings(hide) {
 	chrome.storage.local.set({'hide': hide}, function() {
-		console.log('Side bar setting set to: ' + hide);
+		if(chrome.runtime.lastError !== undefined)
+			console.log('Something went wrong when setting the hide variable in the local storage: ' + chrome.runtime.lastError);
 	});
 }
 
 function updateSidebar() {
 	chrome.storage.local.get('hide', function(result) {
-		console.log('runtime.lastError: ' + chrome.runtime.lastError);
-		console.log('result: ' + result);
-		console.log('hide: ' + result.hide);
-		setSidebar(result.hide);
-		console.log('Done updating sidebar, continuing.');
+		if(chrome.runtime.lastError !== undefined)
+			console.log('Something went wrong when getting the hide variable in the local storage: ' + chrome.runtime.lastError);
+		else
+			setSidebar(result.hide);
 	});
 }
